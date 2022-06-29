7a2558c3afde9805f0e59f65b7704170b5e1ad91
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-06-29 11:14:18 +0300

    Refactor the code

--- predict.py
+++ predict.py
@@ -4,52 +4,65 @@ import sys
 import numpy as np
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
+
 torch.set_num_threads(2)
 MODEL_NAME = 'xlm-roberta-base'
+labels_upper = ['HI', 'ID', 'IN', 'IP', 'LY', 'NA', 'OP', 'SP']
+labels_lower = ['av', 'ds', 'dtp', 'ed', 'en', 'fi', 'it', 'lt',
+                'nb', 'ne', 'ob', 'ra', 're', 'rs', 'rv', 'sr']
+labels_full = labels_upper + labels_lower
+
 
 def argparser():
     ap = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     ap.add_argument('--model_name', default=MODEL_NAME,
                     help='Pretrained model name')
     ap.add_argument('--text', metavar='FILE', required=True,
-                    help='Text to be predicted') #could also be string?
+                    help='Text to be predicted')
     ap.add_argument('--load_model', default=None, metavar='FILE',
                     help='Load model from file')
     ap.add_argument('--threshold', default=0.4, metavar='FLOAT', type=float,
                     help='threshold for calculating f-score')
     ap.add_argument('--labels', choices=['full', 'upper'], default='full')
-    ap.add_argument('--output', default=None, metavar='FILE', help='Location to save predictions')
+    ap.add_argument('--output', default=None, metavar='FILE',
+                    help='Location to save predictions')
     return ap
 
-options = argparser().parse_args(sys.argv[1:])
 
-labels_full = ['HI', 'ID', 'IN', 'IP', 'LY', 'NA', 'OP', 'SP', 'av', 'ds', 'dtp', 'ed', 'en', 'fi', 'it', 'lt', 'nb', 'ne', 'ob', 'ra', 're', 'rs', 'rv', 'sr']
-labels_upper = ['HI', 'ID', 'IN', 'IP', 'LY', 'NA', 'OP', 'SP']
-
-if options.labels == 'full':
-    labels = labels_full
-else:
-    labels = labels_upper
-num_labels = len(labels)
-#print("Number of labels:", num_labels)
-
-# tokenizer from the pretrained model
-tokenizer = transformers.AutoTokenizer.from_pretrained(options.model_name)
-
-# load our fine-tuned model
-model = torch.load(options.load_model, map_location=torch.device('cpu'))
-#model.to('cpu')
-
-def predict_labels(string):
-    tokenized = tokenizer(string, return_tensors='pt')
+def predict_labels(tokenizer, model, labels, threshold, text):
+    tokenized = tokenizer(text, return_tensors='pt')
     pred = model(**tokenized)
     sigmoid = torch.nn.Sigmoid()
     probs = sigmoid(torch.Tensor(pred.logits.detach().numpy()))
     preds = np.zeros(probs.shape)
-    preds[np.where(probs >= options.threshold)] = 1
-    return [labels[idx] for idx, label in enumerate(preds.flatten()) if label >= options.threshold]
+    preds[np.where(probs >= threshold)] = 1
+    return [labels[idx] for idx, label in enumerate(preds.flatten())
+            if label >= threshold]
 
-# predict labels text at a time
-with open(options.text, 'r') as f:
-    for line in f:
-        print(f'{" ".join(predict_labels(line))}')
+
+def load_models(name_tokenizer, name_fine_tuned):
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+            name_tokenizer)
+    model = torch.load(
+            name_fine_tuned, map_location=torch.device('cpu'))
+    return tokenizer, model
+
+
+def main():
+    options = argparser().parse_args(sys.argv[1:])
+    tokenizer, model = load_models(options.model_name, options.load_model)
+    if options.labels == 'full':
+        labels = labels_full
+    else:
+        labels = labels_upper
+
+    with open(options.text, 'r') as infile:
+        for line in infile:
+            predictions = predict_labels(
+                    tokenizer, model, labels, options.threshold, line)
+            print(f'{" ".join(predictions)}')
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
