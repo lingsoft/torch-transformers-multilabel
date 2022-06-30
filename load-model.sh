9115e80da42e87efd4853648b0f046dbeff62131
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-06-30 11:29:44 +0300

    Clarify comments

--- load-model.sh
+++ load-model.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Load trained models
+# Load fine-tuned model
+# Models for two or four languages. Here se stands for Swedish. ;)
+
 mkdir -p models
 #model=xlmr-base-fi-se.pt
 model=xlmr-base-en-fi-fr-se.pt
